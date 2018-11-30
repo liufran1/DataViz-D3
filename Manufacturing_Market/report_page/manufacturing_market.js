8386e79adbb6033e8754675d4919e365e1ae0e13
liufran1 <liufran1@uchicago.edu>
2018-11-30 11:20:53 -0600

    Add data labels

--- Manufacturing_Market/report_page/manufacturing_market.js
+++ Manufacturing_Market/report_page/manufacturing_market.js
@@ -1309,8 +1309,8 @@ manufacturing_data = [
 
 
 // build the SVG 
-svgwidth = 960;
-svgheight = 500;
+svgwidth = 900;
+svgheight = 600;
 margin = {top: 12, right: 12, bottom: 24, left: 24};
 mywidth = svgwidth-margin.left-margin.right;
 myheight = svgheight-margin.top-margin.bottom;
@@ -1321,15 +1321,18 @@ svg = d3.select('.container')
   .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// TODO: function to filter data
+
+// suspect the scaling isn't working because it's being based on the whole dataset
 // build the scales
 //posX = d3.scaleLinear()
 posX = d3.scaleLog()
-    .domain([0, 1000000])
+    .domain([0, 100])
     .domain(d3.extent(manufacturing_data,function(d){return d.Total_cost_of_materials}))
     .range([0, mywidth]);
 //posY = d3.scaleLinear()
 posY = d3.scaleLog()
-    .domain([0, 1000000])
+    .domain([0, 100])
     .domain(d3.extent(manufacturing_data,function(d){return d.Number_of_employees}))
     .range([myheight, 0]);
 sizeX = d3.scaleLinear()
@@ -1340,18 +1343,33 @@ sizeY = d3.scaleLinear()
     .range([0, 1]);
 color = d3.scaleOrdinal(d3.schemeCategory10);
 
-//Bubbles
+// Bubbles
 svg.selectAll('.markers')
   .data(manufacturing_data)
   .enter()
     .append("circle")
-    .attr("class","markers")
-    .attr("cx", function(d){return posX(d.Total_cost_of_materials);})
-    .attr("cy", function(d){return posY(d.Number_of_employees);})
-    .attr("r",  function(d){return sizeX(d.Total_value_of_shipments_and_receipts_for_services);})
-    .attr("fill",function(d){return color(d.Meaning_of_2012_NAICS_code);});
-    
+        .attr("class","markers")
+        .filter(function(d) { return d.Year == 2016 && d["2012_NAICS_code"].length == 3}) 
+        .attr("cx", function(d){return posX(d.Total_cost_of_materials);})
+        .attr("cy", function(d){return posY(d.Number_of_employees);})
+        .attr("r",  function(d){return sizeX(d.Total_value_of_shipments_and_receipts_for_services);})
+        .attr("fill",function(d){return color(d.Meaning_of_2012_NAICS_code);});
+
+// Add data label
+svg.selectAll("text")
+    .data(manufacturing_data)
+    .enter()
+        .append("text")
+        .filter(function(d) { return d.Year == 2016 && d["2012_NAICS_code"].length == 3}) 
+        .attr("x", function(d) { return posX(d.Total_cost_of_materials)-20; })
+        .attr("y", function(d) { return posY(d.Number_of_employees)-10; })
+        .text( function (d) { return d.Meaning_of_2012_NAICS_code; })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("fill", "black");
+
 // make the axes
+
 svg.append("g")
     .attr("class", "axis axis--x")
     .attr("transform", "translate(0," + myheight + ")")
