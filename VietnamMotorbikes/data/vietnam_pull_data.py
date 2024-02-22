19d6036dd50ebfedd63cc5bc446968251fb4d356
liufran1 <liufran1@uchicago.edu>
2024-02-22 12:29:51 -0500

    Initial Vietnam data for vehicles and boundaries

---
+++ VietnamMotorbikes/data/vietnam_pull_data.py
@@ -0,0 +1,36 @@
+import geopandas as gpd
+import pandas as pd
+from shapely.ops import unary_union
+import requests
+
+
+def pull_geo_data():
+    url = 'https://github.com/nguyenduy1133/Free-GIS-Data/blob/c1a1dc5adae1a80ecbff77ae88bd758801004de9/VietNam/Administrative/Provinces_included_Paracel_SpratlyIslands_combine.geojson?raw=true'
+
+    r = requests.get(url)
+
+    with open('provinces.geojson', 'wb') as f:
+      f.write(r.content)
+
+
+    vietnam_districts = gpd.read_file('https://data.opendevelopmentmekong.net/dataset/6f054351-bf2c-422e-8deb-0a511d63a315/resource/78b3fb31-8c96-47d3-af64-d1a6e168e2ea/download/diaphanhuyen.geojson')
+
+    vietnam_district_population = vietnam_districts.copy()
+    vietnam_district_population['point_geom'] = vietnam_district_population['geometry'].centroid
+    vietnam_district_population['geometry'] = vietnam_district_population.apply(lambda x: x['point_geom'].buffer(x['Dan_So']/2000000), axis=1)
+
+    vietnam_gdf = gpd.read_file('provinces.geojson')
+    boundary = gpd.GeoSeries(unary_union(vietnam_gdf['geometry']))
+
+
+    boundary.to_file('vietnam_boundary.geojson')
+    vietnam_district_population[['Ten_Tinh','Ten_Huyen','geometry']].to_file('vietnam_districts.geojson')
+
+
+def format_vehicle_data():
+    df = pd.read_csv('Vietnam Motorbikes - Sheet1.csv') # https://docs.google.com/spreadsheets/d/1icaOCRUSklLu0VCTyINQTLoaDbnm_lO0TZTN0DVHqvM/edit#gid=0
+    df['Total number of registered motorcycles'] = df['Total number of registered motorcycles'].astype('str').str.replace(',','').astype('float').astype('Int64')
+    df['Total number of registered cars'] = df['Total number of registered cars'].astype('str').str.replace(',','').astype('float').astype('Int64')
+    df['Population'] = df['Population'].astype('str').str.replace(',','').astype('float').astype('Int64')
+
+    df.to_csv('VietnamVehicles_1991-2022.csv',index=False)
