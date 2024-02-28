2d55c29b53c4e85336d49d7bb8c95639fd3ab569
Franklin Liu <16155488+liufran1@users.noreply.github.com>
2024-02-28 16:07:58 +0000

    vietnam viz - fixed pm line chart bug

--- VietnamMotorbikes/viz/vietnam_air_quality.js
+++ VietnamMotorbikes/viz/vietnam_air_quality.js
@@ -1,27 +1,27 @@
-let svgPM25 = d3
+let svgPM = d3
   .select("#pmline")
   .append("svg")
   .attr("height", svgHeight) // TO DO - Make adaptive
   .attr("width", svgWidth);
 
-let groupPM25 = svgPM25.append("g");
+let groupPM25 = svgPM.append("g");
 
 d3.csv("data/VietnamVsNYCpmpollution_2023.csv", d3.autoType).then(
-  function (carbonSources) {
+  function (pollutionData) {
     // console.log(vehicleData);
-    
+
     let xScale = d3
       .scaleLinear()
-      .domain(d3.extent(carbonSources, (d) => d["date"]))
+      .domain(d3.extent(pollutionData, (d) => d["date"]))
       .range([0, svgWidth]);
-    
+
     let yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(carbonSources, (d) => d["pm25"])])
+      .domain([0, d3.max(pollutionData, (d) => d["pm25"])])
       .range([svgHeight, 0]);
 
     drawLineChart(
-      carbonSources,
+      pollutionData.filter((d) => d["city"] === "Hanoi"),
       groupPM25,
       xScale,
       yScale,
@@ -30,3 +30,15 @@ d3.csv("data/VietnamVsNYCpmpollution_2023.csv", d3.autoType).then(
       "blue",
       "hanoiPMline",
     );
+    drawLineChart(
+      pollutionData.filter((d) => d["city"] === "New York"),
+      groupPM25,
+      xScale,
+      yScale,
+      "date",
+      "pm25",
+      "orange",
+      "nycPMline",
+    );
+  },
+);
