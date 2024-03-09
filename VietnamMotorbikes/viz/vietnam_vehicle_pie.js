47e05170ec1f4d998087727838abecc62102f11c
Franklin Liu <16155488+liufran1@users.noreply.github.com>
2024-03-09 19:01:03 +0000

    Vietnam viz initial donut chart refactor

--- VietnamMotorbikes/viz/vietnam_vehicle_pie.js
+++ VietnamMotorbikes/viz/vietnam_vehicle_pie.js
@@ -1,19 +1,22 @@
-let pollutantSelect = "CO";
-
 createPollutantPie = function () {
-  d3.csv(
-    "https://raw.githubusercontent.com/liufran1/DataViz-D3/master/VietnamMotorbikes/data/hcmc_vehicle_air_pollutants.csv",
-    d3.autoType,
-  ).then(function (pollutionBreakdown) {
-    const width = 500;
-    const height = Math.min(width, 500);
-    const radius = Math.min(width, height) / 2;
+  const width = 500;
+  const height = Math.min(width, 500);
+  const radius = Math.min(width, height) / 2;
 
-    const arc = d3
-      .arc()
-      .innerRadius(radius * 0.67)
-      .outerRadius(radius - 1);
+  const arc = d3
+    .arc()
+    .innerRadius(radius * 0.67)
+    .outerRadius(radius - 1);
 
+  const svg = d3
+    .select("#vehicle_pie")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height]);
+  // .attr("style", "max-width: 100%; height: auto;");
+
+  let plotPie = function (pollutionBreakdown, pollutant) {
     const pie = d3
       .pie()
       .padAngle(1 / radius)
@@ -26,45 +29,47 @@ createPollutantPie = function () {
       .range(d3.schemeAccent);
     // TO DO: update colors
 
-    const svg = d3
-      .select("#vehicle_pie")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [-width / 2, -height / 2, width, height]);
-    // .attr("style", "max-width: 100%; height: auto;");
-
     // TO DO: Turn this into an update function
-    svg
+
+    // Pie component
+    let pieGroup = svg
       .append("g")
       .selectAll()
-      .data(
-        pie(
-          pollutionBreakdown.filter((d) => d["Pollutant"] == pollutantSelect),
-        ),
-      )
-      .join("path")
-      .attr("fill", (d) => color(d.data["Vehicle_Type"]))
-      .attr("d", arc)
-      .append("title")
-      .text(
-        (d) =>
-          `${d.data["Vehicle_Type"]}: ${d.data[
-            "Pollutant_percent"
-          ].toLocaleString()}`,
-      );
+      .data(pie(pollutionBreakdown.filter((d) => d["Pollutant"] == pollutant)));
 
+    pieGroup.exit().transition().duration(1000).attr("width", 0).remove();
+
+    // pieGroup
+    //   .join("path")
+    //   .enter()
+    //   .merge(pieGroup)
+    //   .transition()
+    //   .attr("fill", (d) => color(d.data["Vehicle_Type"]))
+    //   .attr("d", arc);
+
+    pieGroup
+      .join("path")
+      .merge(pieGroup)
+      .transition()
+      .attr("fill", (d) => color(d.data["Vehicle_Type"]))
+      .attr("d", arc);
+
+    // .append("title")
+    // .text(
+    //   (d) =>
+    //     `${d.data["Vehicle_Type"]}: ${d.data[
+    //       "Pollutant_percent"
+    //     ].toLocaleString()}`,
+    // );
+
+    // text component
     svg
       .append("g")
       .attr("font-family", "sans-serif")
       .attr("font-size", 12)
       .attr("text-anchor", "middle")
       .selectAll()
-      .data(
-        pie(
-          pollutionBreakdown.filter((d) => d["Pollutant"] == pollutantSelect),
-        ),
-      )
+      .data(pie(pollutionBreakdown.filter((d) => d["Pollutant"] == pollutant)))
       .join("text")
       .attr("transform", (d) => `translate(${arc.centroid(d)})`)
       .call((text) =>
@@ -90,17 +95,31 @@ createPollutantPie = function () {
             ),
         // .text(d => d.data['Pollutant_percent'].toLocaleString("en-US"))
       );
-  });
+  };
+
+  d3.csv("./data/hcmc_vehicle_air_pollutants.csv", d3.autoType).then(
+    function (pollutionBreakdown) {
+      plotPie(pollutionBreakdown, "CO");
+    },
+  );
   function update(step) {
     steps[step].call();
   }
 
   var steps = [
     function step0() {
-      // TO DO: data step: animate in bars, highlight SEAsia
+      d3.csv("./data/hcmc_vehicle_air_pollutants.csv", d3.autoType).then(
+        function (pollutionBreakdown) {
+          plotPie(pollutionBreakdown, "CO");
+        },
+      );
     },
     function step1() {
-      // TO DO: data step: fade out everything but Vietnam
+      d3.csv("./data/hcmc_vehicle_air_pollutants.csv", d3.autoType).then(
+        function (pollutionBreakdown) {
+          plotPie(pollutionBreakdown, "PM2.5");
+        },
+      );
     },
     function step2() {},
   ];
