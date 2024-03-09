82adb9eb538563ae83f56b8deecbad8c98327a8b
Franklin Liu <16155488+liufran1@users.noreply.github.com>
2024-03-09 21:57:15 +0000

    Vietnam viz add labels to population growth

--- VietnamMotorbikes/viz/vietnam_population_growth.js
+++ VietnamMotorbikes/viz/vietnam_population_growth.js
@@ -1,32 +1,33 @@
 createPopulationChangeGraphic = function () {
   var endValues = {};
+  var beginValues = {};
+
   d3.csv("data/VietnamVehicles_1991-2022.csv", d3.autoType).then(
     function (vehicleData) {
       // Specify the chart’s dimensions.
-      const width = 500;
-      const height = 500;
-      const marginTop = 10;
-      const marginRight = 10;
-      const marginBottom = 20;
-      const marginLeft = 40;
+      const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+      const svgHeight = 500;
+      const svgWidth = 500;
+      const width = svgWidth - margin.left - margin.right;
+      const height = svgHeight - margin.top - margin.bottom;
 
       const x = d3
         .scaleLinear()
         .domain([0, Math.sqrt(d3.max(vehicleData, (d) => d["Population"]))])
-        .range([marginLeft, width - marginRight]);
+        .range([margin.left, width - margin.right]);
 
       const y = d3
         .scaleLinear()
         .domain([Math.sqrt(d3.max(vehicleData, (d) => d["Population"])), 0])
-        .range([height - marginBottom, marginTop]);
+        .range([height - margin.bottom, margin.top]);
 
       const svg = d3
         .select("#population_growth")
         .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+      // .attr("viewBox", [0, 0, width, height])
+      // .attr("style", "max-width: 100%; height: auto;");
 
       let beginning_data = vehicleData.filter((d) => d["Year"] == 1991)[0];
       let ending_data = vehicleData.filter((d) => d["Year"] == 2022)[0];
@@ -44,6 +45,26 @@ createPopulationChangeGraphic = function () {
         ),
       );
 
+      endValues["totalPop_textx"] = x(
+        (Math.sqrt(ending_data["Population"]) * 3) / 4,
+      );
+      endValues["totalPop_texty"] = y(
+        (Math.sqrt(ending_data["Population"]) * 3) / 4,
+      );
+      endValues["urbanPop_textx"] = x(
+        Math.sqrt(
+          (ending_data["Population"] * ending_data["Urbanization Rate"]) / 200,
+        ),
+      );
+      endValues["urbanPop_texty"] = y(
+        Math.sqrt(
+          (ending_data["Population"] * ending_data["Urbanization Rate"]) / 200,
+        ),
+      );
+      endValues["urbanPop"] =
+        (ending_data["Population"] * ending_data["Urbanization Rate"]) / 100;
+      endValues["totalPop"] = ending_data["Population"];
+
       svg
         .append("rect")
         .attr("fill", "grey")
@@ -51,9 +72,9 @@ createPopulationChangeGraphic = function () {
         .attr("stroke", "black")
         .attr("stroke-width", 0.3)
         .attr("id", "overallPopulation")
-        .attr("y", 0)
+        .attr("y", margin.top)
         .attr("height", y(Math.sqrt(beginning_data["Population"])))
-        .attr("x", 0)
+        .attr("x", margin.left)
         .attr("width", x(Math.sqrt(beginning_data["Population"])));
 
       svg
@@ -63,7 +84,7 @@ createPopulationChangeGraphic = function () {
         .attr("stroke", "black")
         .attr("stroke-width", 0.3)
         .attr("id", "urbanPopulation")
-        .attr("y", 0)
+        .attr("y", margin.top)
         .attr(
           "height",
           y(
@@ -74,7 +95,7 @@ createPopulationChangeGraphic = function () {
             ),
           ),
         )
-        .attr("x", 0)
+        .attr("x", margin.left)
         .attr(
           "width",
           x(
@@ -84,6 +105,62 @@ createPopulationChangeGraphic = function () {
                 100,
             ),
           ),
+        );
+
+      svg
+        .append("text")
+        .attr("transform", "translate(" + width / 2 + "," + margin.top + ")")
+        .attr("id", "population_rects_title")
+        .style("font-size", "15px")
+        .attr("opacity", 1)
+        .text("1991");
+
+      svg
+        .append("text")
+        .attr(
+          "x",
+          x(
+            Math.sqrt(
+              (beginning_data["Population"] *
+                beginning_data["Urbanization Rate"]) /
+                200,
+            ),
+          ),
+        )
+        .attr(
+          "y",
+          y(
+            Math.sqrt(
+              (beginning_data["Population"] *
+                beginning_data["Urbanization Rate"]) /
+                200,
+            ),
+          ),
+        )
+        .style("text-anchor", "middle")
+        .attr("id", "urban_pop_title")
+        .style("font-size", "15px")
+        .attr("opacity", 1)
+        .text(
+          "Urban Population: " +
+            d3.format(".2s")(
+              (beginning_data["Population"] *
+                beginning_data["Urbanization Rate"]) /
+                100,
+            ),
+        );
+
+      svg
+        .append("text")
+        .attr("x", x(Math.sqrt((beginning_data["Population"] * 3) / 4)))
+        .attr("y", y(Math.sqrt((beginning_data["Population"] * 3) / 4)))
+        .style("text-anchor", "middle")
+        .attr("id", "pop_title")
+        .style("font-size", "15px")
+        .attr("opacity", 1)
+        .text(
+          "National Population: " +
+            d3.format(".2s")(beginning_data["Population"]),
         );
 
       // animate in population numbers - https://www.geeksforgeeks.org/how-to-make-animated-counter-using-javascript/
@@ -107,6 +184,27 @@ createPopulationChangeGraphic = function () {
         .duration(500)
         .attr("width", endValues["urbanPop_width"])
         .attr("height", endValues["urbanPop_height"]);
+
+      d3.select("#urban_pop_title")
+        .transition()
+        .duration(500)
+        .attr("x", endValues["urbanPop_textx"])
+        .attr("y", endValues["urbanPop_texty"])
+        .text("Urban Population: " + d3.format(".2s")(endValues["urbanPop"]));
+
+      d3.select("#pop_title")
+        .transition()
+        .duration(500)
+        .attr("x", endValues["totalPop_textx"])
+        .attr("y", endValues["totalPop_texty"])
+        .text(
+          "National Population: " + d3.format(".2s")(endValues["totalPop"]),
+        );
+
+      d3.select("#population_rects_title")
+        .transition()
+        .duration(500)
+        .text("2022");
     },
   ];
 
